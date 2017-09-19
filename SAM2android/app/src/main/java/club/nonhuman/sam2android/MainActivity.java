19039f6b7801b88b6afdff243b42c0ff638c89f9
Arvid Jense <arvid@arvidjense.com>
2017-09-19 18:59:59 +0200

    basic project set up

---
+++ SAM2android/app/src/main/java/club/nonhuman/sam2android/MainActivity.java
@@ -0,0 +1,13 @@
+package club.nonhuman.sam2android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
